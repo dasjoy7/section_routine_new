669dd060fa9958840f0f8956a0600a83362e1fc9
Joy Das <dasjoy8709@gmail.com>
2025-12-25 02:14:31 +0600

    signin signup without verification

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -1,16 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:section_routine/screens/signup_page.dart';
 
-class SigninPage extends StatelessWidget {
+class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
 
   @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool isValidEmail(String email) {
+    String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(email);
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In'),
-      ),
+      backgroundColor: Colors.grey[100],
+
       body: Center(
-        child: Text("This is the signin page"),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.school, size: 64, color: Colors.blue),
+                    SizedBox(height: 16),
+                    Text(
+                      'Welcome Back',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 24),
+
+                    //email field
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    SizedBox(height: 16),
+
+                    //password field
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () {
+                          String email = emailController.text.trim();
+                          String password = passwordController.text.trim();
+
+                          void showWarning(String message){
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: 
+                              Text(message),
+                              backgroundColor: Colors.red,
+                              duration: Duration(seconds:2),
+                              )
+                            );
+                          }
+
+                          if (email.isEmpty ||password.isEmpty) {
+                            showWarning('Please fill all fields');
+                            return;
+                          }
+
+                          else if (!isValidEmail(email)) {
+                            showWarning('Invalid email format');
+                            return;
+                          }
+
+                          else{
+
+                          }
+                        },
+
+                        child: Text('Sign In', style: TextStyle(fontSize: 18)),
+                      ),
+                    ),
+                    SizedBox(height: 16,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account? "),
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const SignupPage(),)
+                              );
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/screens/signup_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:section_routine/screens/signin_page.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  bool isValidEmail(String email) {
+    String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(email);
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.school, size: 64, color: Colors.blue),
+                    SizedBox(height: 16),
+                    Text(
+                      'Create Account',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 24),
+
+                    //full name field
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    SizedBox(height: 16),
+
+                    //email field
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    SizedBox(height: 16),
+
+                    //password field
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 24),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () {
+                          String email = emailController.text.trim();
+                          String password = passwordController.text.trim();
+                          String name = nameController.text.trim();
+
+                          void showWarning(String message){
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: 
+                              Text(message),
+                              backgroundColor: Colors.red,
+                              duration: Duration(seconds:2),
+                              )
+                            );
+                          }
+
+                          if (name.isEmpty || email.isEmpty ||password.isEmpty) {
+                            showWarning('Please fill all fields');
+                            return;
+                          }
+
+                          else if (!isValidEmail(email)) {
+                            showWarning('Invalid email format');
+                            return;
+                          }
+
+                          else{
+
+                          }
+                        },
+
+                        child: Text('Sign Up', style: TextStyle(fontSize: 18)),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const SigninPage(),)
+                              );
+                          },
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
